7fe95e3264be48662b1d7b0d8e72cc85a16698c3
ralf.yang <ralf.yang@gsshop.com>
2014-10-06 09:20:34 +0900

    Reorg.

---
+++ app/src/main/java/com/grevu/intro/IntroActivity.java
@@ -0,0 +1,28 @@
+package com.grevu.intro;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class IntroActivity extends FragmentActivity {
+
+    private FacebookFragment facebookFragment;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (savedInstanceState == null) {
+            // Add the fragment on initial activity setup
+            facebookFragment = new FacebookFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(android.R.id.content, facebookFragment)
+                    .commit();
+        } else {
+            // Or set the fragment from restored state info
+            facebookFragment = (FacebookFragment) getSupportFragmentManager()
+                    .findFragmentById(android.R.id.content);
+        }
+    }
+
+}
